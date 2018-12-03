84f659899f694943ecfb9ec9fbc2696a68b8745c
Benjamin BINARD <benjii.binard@gmail.com>
2018-12-03 09:35:47 +0100

    nouveau fichier : Fenetre.class

--- Fenetre.java
+++ Fenetre.java
@@ -4,56 +4,84 @@ import java.awt.event.*;
 
 public class Fenetre extends JFrame implements ActionListener{
 
-  private JButton bouton;
-  private JTextField texte;
-  private JTextArea textArea;
-  private String message;
-  private Boolean envoi;
+  private JButton butTraduire ;
+  private JButton butEffacer;
+  private JButton butQuitter;
+  private JTextArea ATraduire;
+  private JTextArea Traduction;
+  private String texte;
 
   public Fenetre(int tailleX, int tailleY){
     super();
     this.setSize(tailleX,tailleY);
     this.setVisible(true);
-    this.setTitle("Client - TP4");
+    this.setTitle("Traducteur Javanais");
     this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
     this.placementInterne();
   }
 
   public void placementInterne(){
-    this.envoi=false;
-    this.bouton = new JButton ("OK - Modifier");
-    this.texte = new JTextField("mon message");
-    this.textArea = new JTextArea(5,20);
-    this.textArea.setEditable(false);
-    Container c = this.getContentPane();
-    c.add(this.bouton, BorderLayout.WEST);
-    c.add(this.texte, BorderLayout.CENTER);
-    c.add(this.textArea, BorderLayout.SOUTH);
-    this.bouton.addActionListener(this);
-  }
+    this.butTraduire = new JButton ("Traduire");
+    this.butTraduire.addActionListener(this);
+    this.butEffacer = new JButton ("Effacer");
+    this.butEffacer.addActionListener(this);
+    this.butQuitter = new JButton ("Quitter");
+    this.butQuitter.addActionListener(this);
 
-  public void connecte(String message){
-    this.textArea.append(message+"\n");
+    this.ATraduire = new JTextArea(25,50);
+    this.ATraduire.setEditable(true);
+
+    this.Traduction = new JTextArea(25,50);
+    this.Traduction.setEditable(false);
+
+    /*JPanel bas = new JPanel();
+    bas.add(this.butTraduire);
+    bas.add(this.butEffacer);
+    bas.add(this.butQuitter);
+    getContentPane().add(bas, BorderLayout.SOUTH);
+
+    JPanel gauche = new JPanel();
+    gauche.add(this.ATraduire);
+    getContentPane().add(gauche,BorderLayout.WEST);
+
+    JPanel droite = new JPanel();
+    droite.add(this.Traduction);
+    //droite.add(bas);
+    getContentPane().add(droite,BorderLayout.EAST);*/
+
+
+
+    Panel panel = new Panel();
+    Panel controlPanel = new Panel();
+    panel.setSize(300,300);
+    GridLayout layout = new GridLayout(3,2);
+
+    panel.setLayout(layout);
+    panel.add(this.butTraduire);
+    panel.add(this.butEffacer);
+    panel.add(this.butQuitter);
+    controlPanel.add(panel);
+
+    this.pack();
   }
 
   public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == this.bouton) {
-      this.message = this.texte.getText();
-      System.out.println(this.message);
-      this.envoi=true;
+    if (e.getSource() == this.butTraduire) {
+      this.texte = this.ATraduire.getText();
+      System.out.println(this.texte);
+    }
+    if (e.getSource() == this.butEffacer) {
+      this.ATraduire.setText(null);
+      this.Traduction.setText(null);
+    }
+    if (e.getSource() == this.butQuitter) {
+      System.exit(0);
     }
   }
 
-  public Boolean getEnvoi(){
-    return this.envoi;
-  }
-
-  public String getMessage(){
-    return this.message;
-  }
-
-  public void setEnvoi(Boolean nouvBool){
-    this.envoi=nouvBool;
+  public static void main( String [] args ) {
+   //ClientSimple monClient = new ClientSimple("localhost","8888");
+   Fenetre maFen = new Fenetre(200,400); // Chez Momo
   }
 
 }
--- PrincipalBen.java
+++ PrincipalBen.java
@@ -1,4 +1,1 @@
-public static void main( String [] args ) {
-     //ClientSimple monClient = new ClientSimple("localhost","8888");
-     Fenetre maFen = new Fenetre(200,400); // Chez Momo
-   }
+
